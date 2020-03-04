c30a63e062e14f561b37a972b2cfcc6aeffba6a5
DimitriDeren <50607432+DimitriDeren@users.noreply.github.com>
2020-03-04 19:09:12 +1300

    Add files via upload

---
+++ Stops.java
@@ -0,0 +1,27 @@
+import java.awt.Point;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Stops extends Location {
+
+    public double x;
+    public double y;
+
+    public Stops(double x, double y) {
+        super(x,y);
+        this.x = x;
+        this.y = y;
+    }
+
+    public stopPoint(double x, double y) {
+
+
+    }
+}
